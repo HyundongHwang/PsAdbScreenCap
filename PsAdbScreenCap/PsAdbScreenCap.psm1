3214275575531b4b84525e66b0bff68de3b2cbfa
HyundongHwang <hhd2002@gmail.com>
2018-03-30 15:13:54 +0900

    modify

--- PsAdbScreenCap/PsAdbScreenCap.psm1
+++ PsAdbScreenCap/PsAdbScreenCap.psm1
@@ -19,33 +19,61 @@ function adb-screencap
     Write-Debug "adb shell screencap -p ..."
     adb start-server
     $fileName = "screencapture-$([datetime]::Now.ToString(`"yyMMdd-HHmmss`")).png"
-    $cmd = "cmd.exe /C `"adb shell screencap -p > $fileName`""
-    Invoke-Expression -Command $cmd
-    
-
-    Write-Verbose "ReadAllBytes ..."
-    $fileBytes = [System.IO.File]::ReadAllBytes("$pwd\$fileName")
 
 
-    Write-Verbose "convert ..."
-    $newFileBytes = New-Object System.Collections.Generic.List[byte]
+    [string] $permissionTestStr = (adb shell screencap -p /sdcard/test.png)
+    [bool] $canScreenBufSol = $false
 
-    for($i = 0; $i -lt $fileBytes.Length - 2; $i = $i + 1) 
+    if ($permissionTestStr -like "*error*")
     {
-        if (($fileBytes[$i] -eq 0x0D) -and 
-            ($fileBytes[$i + 1] -eq 0x0D) -and 
-            ($fileBytes[$i + 2] -eq 0x0A))
+        Write-Debug "adb perm error ..."
+        $canScreenBufSol = $true
+    }
+    else 
+    {
+        Write-Debug "adb solution ..."
+        adb shell screencap -p "/sdcard/$fileName"
+        adb pull "/sdcard/$fileName" $fileName
+
+        if ((ls $fileName).Length -eq 0) 
         {
-            $newFileBytes.Add(0x0A)
-            $i = $i + 2
-        }
-        else 
-        {
-            $newFileBytes.Add($fileBytes[$i])
+            $canScreenBufSol = $true
         }
     }
 
-    Write-Verbose "WriteAllBytes ..."
-    [System.IO.File]::WriteAllBytes("$pwd\$fileName", $newFileBytes)
+    if ($canScreenBufSol) 
+    {
+        Write-Debug "screen buffer solution ..."
+
+        $cmd = "cmd.exe /C `"adb shell screencap -p > $fileName`""
+        Invoke-Expression -Command $cmd
+        
+    
+        Write-Verbose "ReadAllBytes ..."
+        $fileBytes = [System.IO.File]::ReadAllBytes("$pwd\$fileName")
+    
+    
+        Write-Verbose "convert ..."
+        $newFileBytes = New-Object System.Collections.Generic.List[byte]
+    
+        for($i = 0; $i -lt $fileBytes.Length - 2; $i = $i + 1) 
+        {
+            if (($fileBytes[$i] -eq 0x0D) -and 
+                ($fileBytes[$i + 1] -eq 0x0D) -and 
+                ($fileBytes[$i + 2] -eq 0x0A))
+            {
+                $newFileBytes.Add(0x0A)
+                $i = $i + 2
+            }
+            else 
+            {
+                $newFileBytes.Add($fileBytes[$i])
+            }
+        }
+    
+        Write-Verbose "WriteAllBytes ..."
+        [System.IO.File]::WriteAllBytes("$pwd\$fileName", $newFileBytes)        
+    }
+
     return "$pwd\$fileName"
 }
